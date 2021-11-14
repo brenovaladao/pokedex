bd36968c82e0f925fa8b6c7a915230a3431ec14f
brenovaladao <brenovvaladao@gmail.com>
2021-11-14 10:44:49 +0100

    First commit - project creation

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Breno Valadão on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
